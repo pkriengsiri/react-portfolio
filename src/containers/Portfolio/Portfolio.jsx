c314bdd19ef1b18d9bcd714401844b7cc0123550
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-18 21:01:24 -0500

    Added containers and components folders and files

---
+++ src/containers/Portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>Portfolio goes here</h1>
+        </div>
+    );
+};
+
+export default Portfolio;
