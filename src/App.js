7eeba6dddcb7b8827a9fd74703c3b94e301db9bb
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-20 16:10:32 -0500

    Added Hashrouter to app.js and scaffolded page structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import About from "./containers/About/About";
+import Portfolio from "./containers/Portfolio/Portfolio";
+import Contact from "./containers/Contact/Contact";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Portfolio goes here</h1>
-      </header>
+      <Header />
+      <Sidebar />
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </HashRouter>
+      <Footer />
     </div>
   );
 }
