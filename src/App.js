ee16f5a229d34703ceb3d04bf4d18e8468ea85a5
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-20 16:25:03 -0500

    Added App.css and global page styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Contact from "./containers/Contact/Contact";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
