433d55b049cc880eb66f1060e4d85ef6429ae2cc
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-08 19:08:21 -0500

    copied files from previous portfolio and switched from hashrouter to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./containers/About/About";
 import Portfolio from "./containers/Portfolio/Portfolio";
-import Contact from "./containers/Contact/Contact";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
+import Contact from "./containers/Contact/Contact"
 
 function App() {
   
@@ -14,13 +14,8 @@ function App() {
     <div className="App">
       <Header />
       <Sidebar />
-      <HashRouter>
-        <Switch>
-          <Route exact path="/" component={About} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          {/* <Route exact path="/contact" component={Contact} /> */}
-        </Switch>
-      </HashRouter>
+      <About />
+
       <Footer />
     </div>
   );
